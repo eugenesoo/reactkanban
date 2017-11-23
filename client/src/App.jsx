36542b28e0911c07edba39dd0d7c5b35c3e59456
Eugene Soo <eugene@soo.sg>
2017-11-23 10:12:58 -0800

    convert to class component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
 
-const App = (props) => (
-  <div>
-    HELLO!
-  </div>
-)
+//setup state, 4 different objects to hold data for 4 columns
+//create a component for each of the column
+//pass down state information into each of the column
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      column1: [
+        {description: 'buy 2 eggs'},
+        {description: 'buy 2 turkeys'},
+      ],
+      column2: [
+        {description: 'buy 2 eggs'},
+        {description: 'buy 2 turkeys'},
+      ],
+      column3: [
+        {description: 'buy 2 eggs'},
+        {description: 'buy 2 turkeys'},
+      ],
+      column4: [
+        {description: 'buy 2 eggs'},
+        {description: 'buy 2 turkeys'},
+      ],
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        HELLO!
+      </div>
+    )
+  }
+}
 
 export default App;
